7bc334884aec661d6e7fd56abdd0fca613e80b4d
Arturo Casco <casco@Emeths-MacBook-Air.local>
2026-03-26 10:34:35 -0600

    feat: se agrega pagina products y navbar para el proyecto

---
+++ audioFly/src/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import logo from '$lib/assets/logo.png'
+    let { children } = $props()
+</script>
+
+<nav>
+    <img class="logo" src={logo} alt="AudioFly">
+    <a href="/">Home</a>
+    <a href="/products">Products</a>
+    <div class="nav-icons">
+        <button>Carrito</button>
+        <button>Login</button>
+    </div>
+</nav>
+
+{@render children()}
+
+<style>
+    nav {
+        display: flex;
+        align-items: center;
+        background-color: #0f0f0f;
+        color: #0f0f0f;
+        gap: 3%;
+    }
+
+    a {
+        color: #f5c518;
+        text-decoration: none;
+        font-size: 200%;
+    }
+
+    .nav-icons {
+        margin-left: auto;
+        background-color: #f5c518;
+        color: #0f0f0f;
+    }
+
+    .logo {
+        width: 10%;
+        height: auto;
+    }
+</style>

18833997c085176e530d02a02b510d45760a8134
Arturo Casco <casco@Emeths-MacBook-Air.local>
2026-03-26 13:20:12 -0600

    feat: pagina stores, logo nuevo, .gitignore

--- audioFly/src/routes/+layout.svelte
+++ audioFly/src/routes/+layout.svelte
@@ -1,14 +1,17 @@
-<script>import "../app.css";
-import logo from '$lib/assets/logo.png'
-import { ShoppingCart } from 'lucide-svelte';
-import { User } from 'lucide-svelte';
-let { children } = $props()</script>
+<script>
+    import "../app.css";
+    import logo from '$lib/assets/logo.png';
+    import { ShoppingCart } from 'lucide-svelte';
+    import { User } from 'lucide-svelte';
+    let { children } = $props();
+</script>
 
 <nav>
     <a href="/">
         <img class="logo" src="{logo}" alt="AudioFly" />
     </a>
     <a href="/products">Products</a>
+    <a href="/stores">Stores</a>
     <div class="nav-icons">
         <button><ShoppingCart /></button>
         <button><User /></button>
@@ -18,24 +21,37 @@ let { children } = $props()</script>
 {@render children()}
 
 <style>
+
+    .logo {
+        width: 5vw;
+        height: auto;
+    }
+
     nav {
         display: flex;
         align-items: center;
         background-color: #0f0f0f;
-        gap: 2%;
+        gap: 2vw;
     }
+
 
     a {
         color: #f5c518;
         text-decoration: none;
-        font-size: 200%;
+        font-size: 2vw;
+    }
+
+    a:hover {
+        font-size: 2.5vw;
     }
 
     .nav-icons {
         color: #f5c518;
+        margin-left: auto;
+        margin-right: 2vw;
     }
 
-    .logo {
-        width: 10%;
+    button:hover {
+        transform: scale(1.2);
     }
 </style>
